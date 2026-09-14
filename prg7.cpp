e46c144b1a7f05457674a390934b77de381cc2ec
dhruvgupta555 <dhruvguptaa.bsr@gmail.com>
2026-09-14 11:10:33 +0530

    Update prg7.cpp

--- prg7.cpp
+++ prg7.cpp
@@ -1,1 +1,34 @@
- 
+ #include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int marks;
+
+public:
+    static int count;
+
+    Student(int m) {
+        marks = m;
+        count++;
+    }
+
+    friend void display(Student s);
+};
+
+int Student::count = 0;
+
+void display(Student s) {
+    cout << "Marks: " << s.marks << endl;
+}
+
+int main() {
+    Student s1(80), s2(90);
+
+    display(s1);
+    display(s2);
+
+    cout << "Total Students: " << Student::count << endl;
+
+    return 0;
+}
