89ea71a188d06e75cce8f3b0094c3a5b12e97634
dhruvgupta555 <dhruvguptaa.bsr@gmail.com>
2026-09-21 10:55:45 +0530

    Create prg5.cpp

---
+++ Lab 2/prg5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void updateSalary(double *salary) {
+    *salary = *salary + (*salary * 0.10);
+}
+
+int main() {
+    double salary = 50000;
+
+    updateSalary(&salary);
+
+    cout << "Updated Salary: " << salary << endl;
+
+    return 0;
+}
